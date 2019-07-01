b004dbf6798135c479fd1d54be76d00e44993b77
henrywycheng <51484937+henrywycheng@users.noreply.github.com>
2019-07-01 23:00:52 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ namespace indenvsensor {
     /* CJMCU-8118 HDC1080 Temp&Humidity sensor addr 0x40 */
     /* CJMCU-8118 CCS811 Gas sensor addr 0x5A */
     //% blockId="indenvStart" block="IndEnv Sensor Start"
-    //% blockGap=2 weight=80
+    //% blockGap=2 weight=79
     export function indenvStart(): boolean {
 	    pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
 	    pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
@@ -58,7 +58,7 @@ namespace indenvsensor {
 	
     /* CJMCU-8118 HDC1080 Temp&Humidity sensor addr 0x40 register 0x00 return 2 bytes */
     //% blockId="indenvgetTemp" block="IndEnv get Temperature"
-    //% blockGap=2 weight=77
+    //% blockGap=2 weight=78
     export function indenvgetTemp(): number {
 	    pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
 	    pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
@@ -72,7 +72,7 @@ namespace indenvsensor {
 
     /* CJMCU-8118 HDC1080 Temp&Humidity sensor addr 0x40 register 0x00 return 2 bytes */
     //% blockId="indenvgetHumid" block="IndEnv get Humidity"
-    //% blockGap=2 weight=76
+    //% blockGap=2 weight=77
     export function indenvgetHumid(): number {
 	  pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
 	  pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
@@ -84,48 +84,9 @@ namespace indenvsensor {
 	  return Math.round(Humidity / 65536 * 100)
     }
 
-    /* CJMCU-8118 CCS811 Gas sensor addr 0x5A register 0x20 return 1 byte */
-    //% blockId="indenvGasIDno" block="IndEnv Gas IDno"
-    //% blockGap=2 weight=75
-    export function indenvGasIDno(): number {
-	    pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
-	    pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
-	    basic.pause(200)
-	    pins.i2cWriteNumber(90,32,NumberFormat.UInt8LE,false)
-	    basic.pause(200)
-	    let GasIDno = pins.i2cReadNumber(90, NumberFormat.UInt8LE, false)
-	    basic.pause(200)
-	    return GasIDno
-    }
-
-    /* CJMCU-8118 CCS811 Gas sensor addr 0x5A register 0xF4 */
-    //% blockId="indenvGasAppStart" block="IndEnv Gas AppStart"
-    //% blockGap=2 weight=74
-    export function indenvGasAppStart() {
-	    pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
-	    pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
-	    basic.pause(200)
-	    pins.i2cWriteNumber(90,244,NumberFormat.UInt8LE,false)
-	    basic.pause(200)
-    }
-
-    /* CJMCU-8118 CCS811 Gas sensor addr 0x5A register 0x00 return 1 byte */
-    //% blockId="indenvGasStatus" block="IndEnv Gas Status"
-    //% blockGap=2 weight=73
-    export function indenvGasStatus(): number {
-	    pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
-	    pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
-	    basic.pause(200)
-	    pins.i2cWriteNumber(90,0,NumberFormat.UInt8LE,false)
-	    basic.pause(200)
-	    let GasStatus = pins.i2cReadNumber(90, NumberFormat.UInt8LE, false)
-	    basic.pause(200)
-	    return GasStatus
-    }
-
     /* CJMCU-8118 CCS811 read eCO2 addr 0x5A register 0x02 return 2 bytes */
     //% blockId="indenvgeteCO2" block="IndEnv get eCO2"
-    //% blockGap=2 weight=72
+    //% blockGap=2 weight=76
     export function indenvgeteCO2(): number {
 	    pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
 	    pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
@@ -137,7 +98,7 @@ namespace indenvsensor {
 
     /* CJMCU-8118 CCS811 read VTOC addr 0x5A register 0x02 return 2 bytes */
     //% blockId="indenvgetTVOC" block="IndEnv get TVOC"
-    //% blockGap=2 weight=71
+    //% blockGap=2 weight=75
     export function indenvgetTVOC(): number {
 	    pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
 	    pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
@@ -145,6 +106,37 @@ namespace indenvsensor {
 	    pins.i2cWriteNumber(90,2,NumberFormat.UInt8LE,false)
 	    basic.pause(200)
 	    return (pins.i2cReadNumber(90, NumberFormat.UInt32BE, false) % 65536)
+    }
+
+
+    /* CJMCU-8118 CCS811 Gas sensor addr 0x5A register 0x00 return 1 byte */
+    //% blockId="indenvGasStatus" block="IndEnv Gas Status"
+    //% blockGap=2 weight=74
+    export function indenvGasStatus(): number {
+	    pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
+	    pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
+	    basic.pause(200)
+	    pins.i2cWriteNumber(90,0,NumberFormat.UInt8LE,false)
+	    basic.pause(200)
+	    let GasStatus = pins.i2cReadNumber(90, NumberFormat.UInt8LE, false)
+	    basic.pause(200)
+	    return GasStatus
+    }
+
+
+    /* CJMCU-8118 CCS811 Gas sensor Data Ready, Gas Status bit3=1 */
+    //% blockId="indenvGasReady" block="IndEnv Gas Data Ready"
+    //% blockGap=2 weight=73
+    export function indenvGasReady(): boolean {
+	    pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
+	    pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
+	    basic.pause(200)
+	    pins.i2cWriteNumber(90,0,NumberFormat.UInt8LE,false)
+	    basic.pause(200)
+	    if ((pins.i2cReadNumber(90, NumberFormat.UInt8LE, false) % 16) !=8) {
+		    return false
+	    }
+	    return true
     }
 
 }
