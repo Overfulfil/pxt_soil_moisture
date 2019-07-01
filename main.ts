2dda531d4a904d2e1afa3992092cd09d9905123d
henrywycheng <51484937+henrywycheng@users.noreply.github.com>
2019-07-01 11:00:26 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -136,7 +136,7 @@ namespace indenvsensor {
     }
 
     /* CJMCU-8118 CCS811 read VTOC addr 0x5A register 0x02 return 2 bytes */
-/*    //% blockId="indenvgetTVOC" block="IndEnv get TVOC"
+    //% blockId="indenvgetTVOC" block="IndEnv get TVOC"
     //% blockGap=2 weight=71
     export function indenvgetTVOC(): number {
 	    pins.setPull(DigitalPin.P19, PinPullMode.PullUp)
@@ -144,7 +144,7 @@ namespace indenvsensor {
 	    basic.pause(200)
 	    pins.i2cWriteNumber(90,2,NumberFormat.UInt8LE,false)
 	    basic.pause(200)
-	    return pins.i2cReadNumber(90, NumberFormat.UInt32BE, false) % 65536
+	    return pins.i2cReadNumber(90, NumberFormat.UInt32BE, false)
     }
-*/
+
 }
