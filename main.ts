e47e429d666a9578c1f6f2523dfbf5387e77f517
larryyee2005 <32700530+larryyee2005@users.noreply.github.com>
2020-04-06 22:25:37 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,28 @@
-//% weight=0 color=#3CB371 icon="\uf140" block="Indoor Environment Sensor"
+//% weight=0 color=#3CB371 icon="\uf140" block="Soil Moisture Sensor"
 
 namespace indenvsensor {
 
-    //% blockId="readMoisture" block="Soil Moisture"
+    //% blockId="readMoisture" block="Soil Moisture (0-4096)"
     //% blockGap=2 weight=79
     export function readMoisture(): number {
    	pins.i2cWriteNumber(81,0,NumberFormat.Int8LE,false)
-	return (pins.i2cReadNumber(81, NumberFormat.UInt16BE, false))    
+	return (4096 - pins.i2cReadNumber(81, NumberFormat.UInt16BE, false))    
+    }
+	
+    //% blockId="SoilTooDry" block="Soil too dry?"
+    //% blockGap=2 weight=79
+    export function SoilTooDry(): boolean {
+   	pins.i2cWriteNumber(81,0,NumberFormat.Int8LE,false)
+	if ((4096 - pins.i2cReadNumber(81, NumberFormat.UInt16BE, false)) < 100) 
+		return ture    
+    }
+
+    //% blockId="SoilTooWet" block="Soil too Wet?"
+    //% blockGap=2 weight=79
+    export function SoilTooWet(): boolean {
+   	pins.i2cWriteNumber(81,0,NumberFormat.Int8LE,false)
+	if ((4096 - pins.i2cReadNumber(81, NumberFormat.UInt16BE, false)) > 3000) 
+		return ture    
     }
 
 }
